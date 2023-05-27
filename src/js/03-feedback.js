b6693657982a04bd670cf6b86b0b54fdda6aea7d
SheVredina <122560380+SheVredina@users.noreply.github.com>
2023-05-27 08:59:55 +0200

    final_8

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,40 +1,34 @@
-import throttle from 'lodash.throttle'
+import throttle from 'lodash.throttle';
 
 const formEl = document.querySelector('.feedback-form');
 const emailEl = document.querySelector('input');
 const messageEl = document.querySelector('textarea');
-const FORMS_KEY = "feedback-form-state";
+const FORMS_KEY = 'feedback-form-state';
 
 formEl.addEventListener('submit', onSubmitForm);
-// emailEl.addEventListener('input', onInputForm);
 formEl.addEventListener('input', throttle(onInputForm, 500));
 saveText();
 
-function onSubmitForm(event){
-    event.preventDefault(); 
-    const { email, message } = event.currentTarget.elements;
-           
-        console.log({email: email.value, message: message.value} );
-        formEl.reset();
-        localStorage.removeItem(FORMS_KEY);
-    }
-  const data = {};
+function onSubmitForm(event) {
+  event.preventDefault();
+  const { email, message } = event.currentTarget.elements;
 
-    function onInputForm(event){
-        // const dataLocal = JSON.parse(localStorage.getItem(FORMS_KEY));
-        // const data = {[event.target.name]:event.target.value, ...dataLocal};
-        // const valueInput = event.currentTarget.value;
-        data[event.target.name] = event.target.value;
-        localStorage.setItem(FORMS_KEY, JSON.stringify(data));
+  console.log({ email: email.value, message: message.value });
+  formEl.reset();
+  localStorage.removeItem(FORMS_KEY);
+}
+const data = {};
 
+function onInputForm(event) {
+  data[event.target.name] = event.target.value;
+  localStorage.setItem(FORMS_KEY, JSON.stringify(data));
 }
 
-function saveText(){
-const savedMessege = JSON.parse(localStorage.getItem(FORMS_KEY));
-if (savedMessege){
+function saveText() {
+  const savedMessege = JSON.parse(localStorage.getItem(FORMS_KEY));
+  if (savedMessege) {
     messageEl.value = savedMessege.message;
     emailEl.value = savedMessege.email;
     console.log('savedMessege');
+  }
 }
-}
-
